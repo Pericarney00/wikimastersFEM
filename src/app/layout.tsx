8f4aa5eb2a9b52e7b36358a4d7dfada48d7d040d
pericarney00 <pericarney@gmail.com>
2026-01-06 12:56:42 -0600

    updated app layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,11 @@
 import { StackProvider, StackTheme } from "@stackframe/stack";
+import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { stackClientApp } from "../stack/client";
 import "./globals.css";
-import { NavBar } from "@/components/nav-bar";
+import {NavBar} from "@/components/nav-bar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -34,6 +36,8 @@ export default function RootLayout({
           <StackTheme>
             <NavBar />
             {children}
+            <Analytics />
+            <SpeedInsights />
           </StackTheme>
         </StackProvider>
       </body>
